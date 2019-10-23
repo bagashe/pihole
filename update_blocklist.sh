2da6ce58ef692d556b6a2d44b3b533149c06ce0d
Bhal Agashe <bhalchandra@gmail.com>
2019-10-23 11:50:30 -0700

    Simple bash script to pull latest blocklist collection.

---
+++ update_blocklist.sh
@@ -0,0 +1,13 @@
+#!/bin/bash -e
+
+# Passwordless `sudo` needs to be enabled for the user who will run this script.
+# By default ...
+#   * The user `pi` on Raspbian has passwordless sudo enabled.
+#   * The user `ubuntu` on EC2 has passwordless sudo enabled.
+# If you need to, you can enable passwordless sudo, by using the `visudo` command as follows.
+# 1. `sudo visudo`
+# 2. Add the following line at the end of the file - `$USER ALL=(ALL) NOPASSWD: ALL`
+# 3. Save and exit. Now you have passwordless sudo for `$USER`
+
+sudo /usr/bin/curl -# https://raw.githubusercontent.com/bagashe/pihole/master/adlists.list -o /etc/pihole/adlists.list
+/usr/local/bin/pihole -g
